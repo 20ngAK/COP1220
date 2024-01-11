4aa1c91efeecf7581eda7b23a40f02a8bc6b1f8b
AK <62631642+20ngAK@users.noreply.github.com>
2024-01-11 16:30:09 -0500

    Add files via upload
    Assignment 1-1 PersonalInfo

---
+++ PersonalInfo.c
@@ -0,0 +1,37 @@
+// COP-1220 Section 86392 
+// Assignment 1-1 - PersonalInfo
+// Display a person's name, birthdate, work phone number, and cell phone number on separate lines.
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char name[30];
+  char birth_date[15];
+  char work_phone[15];
+  char cell_phone[15];
+
+// Remember to use fgets() instead of scanf(), because of space delimiter issue (name char) and buffer overlow protection!
+  
+  printf("Enter your name: ");
+  fgets(name, 50, stdin);
+
+  printf("Enter your birthdate: ");
+  fgets(birth_date, 15, stdin);
+
+  printf("Enter your work phone number: ");
+  fgets(work_phone, 15, stdin);
+
+  printf("Enter your cell phone number: ");
+  fgets(cell_phone, 15, stdin);
+
+  printf("%s", name);
+  printf("%s", birth_date);
+  printf("%s", work_phone);
+  printf("%s", cell_phone);
+
+  return 0;
+}
+
+  
+
