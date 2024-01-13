f6b4f7538992afbef83de2822ea198d99c447527
AK <62631642+20ngAK@users.noreply.github.com>
2024-01-13 10:47:08 -0500

    Update Lyrics.c

--- Lyrics.c
+++ Lyrics.c
@@ -7,7 +7,7 @@
 
 int main() {
   char lyric1[200] = "Oh, well imagine /\nAs I'm pacing the pews in a church corridor /\nAnd I can't help but to hear /\nNo, I can't help but to hear an exchanging of words...";
-  char lyric2[200] = "\nOh-oh-oh-oh-oh;\noh-oh-oh-oh,\noh-oh-oh /\nCaught in a bad romance...";
+  char lyric2[200] = "\nOh-oh-oh-oh-oh,\noh-oh-oh-oh,\noh-oh-oh /\nCaught in a bad romance...";
   char lyric3[200] = "\nComing out my cage\nAnd I've been doing just fine\nGotta gotta be down\nBecause I want it all";
   char lyric4[200] = "\nNow, this is a story all about how\nMy life got flipped-turned upside down\nAnd I'd like to take a minute\nJust sit right there\nI'll tell you how I became the prince of a town called Bel-Air";
 
